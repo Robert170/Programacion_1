8eb076ca97d81be887f2c6c17c3865061b20893a
Robert170 <42983312+Robert170@users.noreply.github.com>
2019-01-25 23:54:02 -0600

    capitulo 5

---
+++ Capitulo_5/Capitulo_5/Capitulo_5.cpp
@@ -0,0 +1,43 @@
+#include "Includes.h"
+
+
+int main()
+{
+	int Eleccion;
+	cout << "Escriba un numero dependiendo del ejercicio que quieras ver" << endl;
+	cin >> Eleccion;
+
+	switch (Eleccion)
+	{
+	case 1:
+
+	case 2:
+
+	case 3:
+
+	case 4:
+	}
+	return 0;
+}
+
+
+int area(int Largo, int Ancho)
+{
+
+}
+
+
+void Try1()
+{
+	//int s1 = area(7; // error: ) missing
+	//int s2 = area(7) // error: ; missing
+	//Int s3 = area(7); // error: Int is not a type
+	//int s4 = area('7); // error: non-terminated character ( ' missing)
+}
+
+void Try2()
+{
+	//int x0 = arena(7); // error: undeclared function
+	//int x1 = area(7); // error: wrong number of arguments
+	//int x2 = area("seven", 2); // error: 1st argument has a wrong type
+}
